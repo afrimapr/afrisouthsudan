5ebf260a8127590d2b2bd1147a176dd09f016b00
AndySouth <southandy@gmail.com>
2021-07-09 13:36:36 +0100

    rename & start afrisouthsudan-creation.R

---
+++ data-raw/afrisouthsudan-creation.R
@@ -0,0 +1,14 @@
+## afrisouthsudan-creation.R
+
+## what did I do to create package
+
+## Github first repo
+
+## RStudio create new project from repo
+
+## usethis::create_package(path=getwd())
+
+## usethis::use_data_raw()
+
+## rename DATASET.R to this afrisouthsudan-creation.R
+
